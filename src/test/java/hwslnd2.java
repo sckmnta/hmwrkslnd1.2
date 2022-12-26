bfec43e90d86785cb70f724032338a66a777886b
Nikita Maniakin <scksckscksckscksck@gmail.com>
2022-12-27 00:32:06 +0300

    optional homework added

--- src/test/java/hwslnd2.java
+++ src/test/java/hwslnd2.java
@@ -1,10 +1,20 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.conditions.webdriver.CurrentFrameUrl;
+import com.codeborne.selenide.conditions.webdriver.CurrentFrameUrlContaining;
+import com.codeborne.selenide.conditions.webdriver.Url;
+import com.codeborne.selenide.conditions.webdriver.UrlContaining;
+import com.google.errorprone.annotations.CheckReturnValue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import javax.annotation.Nonnull;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class hwslnd2 {
     @BeforeAll
@@ -24,5 +34,11 @@ public class hwslnd2 {
         $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
         /// check if it works
         $(".application-main").shouldHave(text("GitHub for enterprises"));
+        /// далее попытался сравнить по ожидаемому и выдаваемому урл, но не вышло
+        //String url = "https://github.com/enterprise";
+        //String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        //assertEquals(url, currentUrl);
+
+
     }
 }
