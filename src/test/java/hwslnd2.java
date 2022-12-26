fc310ed8fcdec4f77654678a1bad1373face549b
Nikita Maniakin <scksckscksckscksck@gmail.com>
2022-12-26 22:27:20 +0300

    changes

--- src/test/java/hwslnd2.java
+++ src/test/java/hwslnd2.java
@@ -1,3 +1,25 @@
-public class hwslnd2 {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class hwslnd2 {
+    @BeforeAll
+    static void beforeall() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        // Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://github.com/";
+    }
+    @Test
+    void scssflTest() {
+        open("");
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+
+
+    }
 }
